1fc6de9c911d23f8518ef15ab4edb859b83980b9
noahblob <noahhagardent@gmail.com>
2024-01-04 19:45:54 +1300

    Optimized solution 380

--- 301-400/Solution380.java
+++ 301-400/Solution380.java
@@ -1,46 +1,29 @@
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
 class Solution380 {
     class RandomizedSet {
         Set<Integer> randomSet;
-        int size;
+        Random rand = new Random();
 
         public RandomizedSet() {
             randomSet = new HashSet<>();
-            size = 0;
         }
 
         public boolean insert(int val) {
-            if (randomSet.add(val)) {
-                size++;
-                return true;
-            } else {
-                return false;
-            }
+            return randomSet.add(val);
         }
 
         public boolean remove(int val) {
-            if (randomSet.contains(val)) {
-                randomSet.remove(val);
-                size--;
-                return true;
-            }
-            return false;
+            return randomSet.remove(val);
         }
 
         public int getRandom() {
-
             List<Integer> arr = new ArrayList<>(randomSet);
-            if (randomSet.size() <= 1) {
-                return arr.get(0);
-            }
-
-            int index = (int) (Math.random() * (size));
-            return arr.get(index);
+            return arr.get(rand.nextInt(arr.size()));
         }
     }
-
 }
