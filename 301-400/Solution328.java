cb18c900883b24673698d4082e10cef0c5d40a70
noahblob <noahhagardent@gmail.com>
2024-03-15 12:23:51 +1300

    Added solution 328

---
+++ 301-400/Solution328.java
@@ -0,0 +1,36 @@
+public class Solution328 {
+  public ListNode oddEvenList(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+    ListNode odd = head;
+    ListNode even = head.next;
+    ListNode evenhead = even;
+    while (even != null && even.next != null) {
+      odd.next = odd.next.next;
+      even.next = even.next.next;
+      odd = odd.next;
+      even = even.next;
+    }
+    odd.next = evenhead;
+    return head;
+  }
+}
+
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {
+  }
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
